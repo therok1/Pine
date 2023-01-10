291a72a776172ecbe7b2c423812f78069570daba
Rok Oblak <oblak2003@gmail.com>
2023-01-10 20:00:03 +0100

    Fixed issues regarding rendering alpha channels

--- Pine/src/Platform/OpenGL/OpenGLRendererAPI.cpp
+++ Pine/src/Platform/OpenGL/OpenGLRendererAPI.cpp
@@ -32,7 +32,7 @@ namespace Pine
 
 	void OpenGLRendererAPI::DrawIndexed(const Ref<VertexArray>& vertexArray, uint32_t indexCount)
 	{
-		uint32_t count = indexCount ?  indexCount : vertexArray->GetIndexBuffer()->GetCount();
+		uint32_t count = indexCount ? indexCount : vertexArray->GetIndexBuffer()->GetCount();
 		glDrawElements(GL_TRIANGLES, count, GL_UNSIGNED_INT, nullptr);
 		glBindTexture(GL_TEXTURE_2D, 0);
 	}
