be829a79e2ba5d722cda5b65e3c3ee34dd7364d1
Rok Oblak <oblak2003@gmail.com>
2023-02-27 20:39:52 +0100

    Updated logger

--- Pine/src/Pine/Core/Log.cpp
+++ Pine/src/Pine/Core/Log.cpp
@@ -1,7 +1,8 @@
 #include "pnpch.h"
 #include "Log.h"
 
-#include "spdlog/sinks/stdout_color_sinks.h"
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
 
 namespace Pine
 {
@@ -10,11 +11,22 @@ namespace Pine
 
 	void Log::Init()
 	{
-		spdlog::set_pattern("%^[%T] %n: %v%$");
-		s_CoreLogger = spdlog::stderr_color_mt("PINE");
-		s_CoreLogger->set_level(spdlog::level::trace);
+		std::vector<spdlog::sink_ptr> logSinks;
 
-		s_ClientLogger = spdlog::stderr_color_mt("APP");
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Pine.log", true));
+
+		logSinks[0]->set_pattern("%^[%T] [%n]: %v%$");
+		logSinks[1]->set_pattern("[%T] [%l] [%n]: %v");
+
+		s_CoreLogger = std::make_shared<spdlog::logger>("PINE", begin(logSinks), end(logSinks));
+		spdlog::register_logger(s_CoreLogger);
+		s_CoreLogger->set_level(spdlog::level::trace);
+		s_CoreLogger->flush_on(spdlog::level::trace);
+
+		s_ClientLogger = std::make_shared<spdlog::logger>("APP", begin(logSinks), end(logSinks));
+		spdlog::register_logger(s_ClientLogger);
 		s_ClientLogger->set_level(spdlog::level::trace);
+		s_ClientLogger->flush_on(spdlog::level::trace);
 	}
 }
