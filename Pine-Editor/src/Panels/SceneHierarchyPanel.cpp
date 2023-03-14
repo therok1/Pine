cc3aadf1e98cd48ebe7eff91d18c115c80640d7b
Rok Oblak <oblak2003@gmail.com>
2023-03-14 21:08:01 +0100

    Reading/writing C# fields in editor

--- Pine-Editor/src/Panels/SceneHierarchyPanel.cpp
+++ Pine-Editor/src/Panels/SceneHierarchyPanel.cpp
@@ -452,7 +452,7 @@ namespace Pine
 		);
 
 		DrawComponent<ScriptComponent>("Script", entity, 
-			[](auto& component)
+			[entity](auto& component) mutable
 			{
 				bool scriptClassExists = ScriptEngine::EntityClassExists(component.ClassName);
 
@@ -464,6 +464,24 @@ namespace Pine
 
 				if (ImGui::InputText("Class", buffer, sizeof(buffer)))
 					component.ClassName = buffer;
+
+				Ref<ScriptInstance> scriptInstance = ScriptEngine::GetEntityScriptInstance(entity.GetUUID());
+				if (scriptInstance)
+				{
+					const auto& fields = scriptInstance->GetScriptClass()->GetFields();
+
+					for (const auto& [name, field] : fields)
+					{
+						if (field.Type == ScriptFieldType::Float)
+						{
+							float data = scriptInstance->GetFieldValue<float>(name);
+							if (ImGui::DragFloat(name.c_str(), &data))
+							{
+								scriptInstance->SetFieldValue(name, data);
+							}
+						}
+					}
+				}
 
 				if (!scriptClassExists)
 					ImGui::PopStyleColor();
--- Pine/src/Pine/Scripting/ScriptEngine.h
+++ Pine/src/Pine/Scripting/ScriptEngine.h
@@ -5,6 +5,7 @@
 
 #include <filesystem>
 #include <string>
+#include <map>
 
 extern "C" 
 {
@@ -13,10 +14,29 @@ extern "C"
 	typedef struct _MonoMethod MonoMethod;
 	typedef struct _MonoAssembly MonoAssembly;
 	typedef struct _MonoImage MonoImage;
+	typedef struct _MonoClassField MonoClassField;
 }
 
 namespace Pine
 {
+	enum class ScriptFieldType
+	{
+		None = 0,
+		Float, Double,
+		Bool, Char, Byte, Short, Int, Long,
+		UByte, UShort, UInt, ULong,
+		Vector2, Vector3, Vector4,
+		Entity
+	};
+
+	struct ScriptField
+	{
+		ScriptFieldType Type;
+		std::string Name;
+
+		MonoClassField* ClassField;
+	};
+
 	class ScriptClass
 	{
 	public:
@@ -28,12 +48,18 @@ namespace Pine
 		MonoMethod* GetMethod(const std::string& name, int parameterCount);
 		MonoObject* InvokeMethod(MonoObject* instance, MonoMethod* method, void** params = nullptr);
 
+		const std::map<std::string, ScriptField>& GetFields() const { return m_Fields; }
+
 	private:
 
 		std::string m_ClassNamespace;
 		std::string m_ClassName;
 
+		std::map<std::string, ScriptField> m_Fields;
+
 		MonoClass* m_MonoClass = nullptr;
+
+		friend class ScriptEngine;
 
 	};
 
@@ -46,6 +72,29 @@ namespace Pine
 		void InvokeOnCreate();
 		void InvokeOnUpdate(float ts);
 
+		Ref<ScriptClass> GetScriptClass() { return m_ScriptClass; }
+
+		template<typename T>
+		T GetFieldValue(const std::string& name)
+		{
+			bool success = GetFieldValueInternal(name, s_FieldValueBuffer);
+			if (!success)
+				return T();
+
+			return *reinterpret_cast<T*>(s_FieldValueBuffer);
+		}
+
+		template<typename T>
+		void SetFieldValue(const std::string& name, const T& value)
+		{
+			SetFieldValueInternal(name, &value);
+		}
+
+	private:
+
+		bool GetFieldValueInternal(const std::string& name, void* buffer);
+		bool SetFieldValueInternal(const std::string& name, const void* value);
+
 	private:
 
 		Ref<ScriptClass> m_ScriptClass;
@@ -54,6 +103,9 @@ namespace Pine
 		MonoMethod* m_Constructor = nullptr;
 		MonoMethod* m_OnCreateMethod = nullptr;
 		MonoMethod* m_OnUpdateMethod = nullptr;
+
+		inline static char s_FieldValueBuffer[8];
+
 	};
 
 	class ScriptEngine
@@ -74,6 +126,8 @@ namespace Pine
 		static void OnUpdateEntity(Entity entity, Timestep ts);
 
 		static Scene* GetSceneContext();
+		static Ref<ScriptInstance> GetEntityScriptInstance(UUID entityID);
+
 		static std::unordered_map<std::string, Ref<ScriptClass>> GetEntityClasses();
 
 		static MonoImage* GetCoreAssemblyImage();
